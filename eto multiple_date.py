37c28d67f392776c8c12fcaa2a3f71aea6f89247
rarayukawa <r1u2u18@gmail.com>
2021-05-06 14:48:33 +0900

    finish 20

--- eto multiple_date.py
+++ eto multiple_date.py
@@ -1,3 +1,6 @@
 eto_list = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥', ]
-eto_name = eto_list[2]
-print('あなたの干支は', eto_name, 'です。')
+eto_name = eto_list[1]
+print('remove()実行前の干支リスト１番は', eto_name, 'です。')
+eto_list.remove('丑')
+eto_name = eto_list[1]
+print('remove()実行後の干支リスト１番は', eto_name, 'です。')
