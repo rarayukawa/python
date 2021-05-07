5ae4e0ba71012353c67cbff4e946f50451110746
rarayukawa <r1u2u18@gmail.com>
2021-05-07 14:07:42 +0900

    finish 35

--- pybot.py
+++ pybot.py
@@ -1,11 +1,20 @@
-while True:
+bot_dict = {
+    'こんにちは: 'コンニチハ',
+    'ありがとう': 'アリガトウ',
+    'さようなら': 'サヨウナラ',
+}
+
+while_True:
     command = input('pybot> ')
-    if 'こんにちは' in command:
-        print('コンニチハ')
-    elif 'ありがとう' in command:
-        print('ドウイタシマシテ')
-    elif 'さようなら' in command:
-        print('サヨウナラ')
+    response = ''
+    for message in bot_dict:
+        if message in command:
+            response = bot_dict[message]
+            break
+
+    if not response:
+        response = 'ナニヲイッテルカワカリマセン'
+    print(response)
+
+    if 'さようなら' in command:
         break
-    else:
-        print('ナニヲイッテルカワカラナイデス')
