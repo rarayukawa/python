10f889d8de95304a1c87b549a5e0f5092349250b
rarayukawa <r1u2u18@gmail.com>
2021-05-07 14:46:37 +0900

    finish 38

--- pybot.py
+++ pybot.py
@@ -1,3 +1,9 @@
+def len_command(command):
+    cmd, text = command.split()
+    length = len(text)
+    response = '文字列ノ長サハ {} 文字デス'.format(length)
+    return response
+
 def heisei_command(command):
     heisei, year_str = command.split()
     year = int(year_str)
@@ -29,13 +35,8 @@ while True:
             break
     if '平成' in command:
         response = heisei_command(command)
-        heisei, year_str = command.split()
-        year = int(year_str)
-        if year >= 1989:
-            heisei_year = year - 1988
-            response = '西暦{}年ハ、平成{}年デス'.format(year, heisei_year)
-        else:
-            response = '西暦{}年ハ、平成デハアリマセン'.format(year)
+    if '長さ' in command:
+        response = len_command(command)
 
     if not response:
         response = "ナニヲイッテイルノカワカリマセン"
