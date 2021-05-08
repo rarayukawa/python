c283e142a1aca663aabc9b7afb485281606aafca
rarayukawa <r1u2u18@gmail.com>
2021-05-08 14:08:21 +0900

    finish 51

---
+++ pybot_weather.py
@@ -0,0 +1,14 @@
+import requests
+
+def weather_command():
+    base_url = 'https://weather.tsukumijima.net/api/forecast'
+    city_code = '130010'
+    url = '{}?city={}'.format(base_url, city_code)
+    r = requests.get(url)
+    weather_data = r.json()
+    city = weather_data['location']['city']
+    label = weather_data['forecasts'][0]['dateLabel']
+    telop = weather_data['forecasts'][0]['telop']
+
+    response = '{}の{}の天気は「{}」です'.format(city, label, telop)
+    return response
