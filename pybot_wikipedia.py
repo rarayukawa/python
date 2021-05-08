5c01f63baab32d5064d586969f02ec1d02f147c0
rarayukawa <r1u2u18@gmail.com>
2021-05-08 14:23:19 +0900

    finish 52

---
+++ pybot_wikipedia.py
@@ -0,0 +1,13 @@
+import wikipedia
+
+def wikipedia_command(command):
+    cmd, keyword = command.split(maxsplit=1)
+    wikipedia.set_lang('ja')
+    try:
+        page = wikipedia.page(keyword)
+        title = page.title
+        summary = page.summary
+        response = 'タイトル: {}\n{}'.format(title, summary)
+    except wikipedia.exceptions.PageError:
+        response = '「{}」の意味が見つかりませんでした'.format(keyword)
+    return response
